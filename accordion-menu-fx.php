3695ec0a351c4262b53536e3d21c8ca48ff88598
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-23 14:58:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/accordion-menu-fx/trunk@279749 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ accordion-menu-fx.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Plugin Name: Accordion Menu FX
+Plugin URI: http://www.flashxml.net/accordion-menu.html
+Description: Probably the most advanced Accordion Menu on the web. Fully XML customizable, without using Flash. And it's free!
+Version: 0.2.0
+Author: FlashXML.net
+Author URI: http://www.flashxml.net/
+License: GPL2
+*/
+
+/* start global parameters */
+	$accordionmenufx_params = array(
+		'count'	=> 0, // number of Accordion Menu FX embeds
+	);
+/* end global parameters */
+
+/* start client side functions */
+	function accordionmenufx_get_embed_code($accordionmenufx_attributes) {
+		global $accordionmenufx_params;
+		$accordionmenufx_params['count']++;
+
+		$plugin_dir = get_option('accordionmenufx_path');
+		if ($plugin_dir === false) {
+			$plugin_dir = 'flashxml/accordion-menu-fx';
+		}
+		$plugin_dir = trim($plugin_dir, '/');
+
+		$settings_file_name = !empty($accordionmenufx_attributes[2]) ? $accordionmenufx_attributes[2] : 'settings.xml';
+		$settings_file_path = WP_CONTENT_DIR . "/{$plugin_dir}/{$settings_file_name}";
+
+		if (function_exists('simplexml_load_file')) {
+			if (file_exists($settings_file_path)) {
+				$data = simplexml_load_file($settings_file_path);
+				$width = (int)$data->General_Properties->componentWidth->attributes()->value;
+				$height = (int)$data->General_Properties->componentHeight->attributes()->value;
+			}
+		} elseif ((int)$accordionmenufx_attributes[4] > 0 && (int)$accordionmenufx_attributes[6] > 0) {
+			$width = $accordionmenufx_attributes[4];
+			$height = $accordionmenufx_attributes[6];
+		} else {
+			return '<!-- invalid Accordion Menu FX width and / or height -->';
+		}
+
+		if ($width == 0 || $height == 0) {
+			return '';
+		}
+
+		$swf_embed = array(
+			'width' => $width,
+			'height' => $height,
+			'text' => trim($accordionmenufx_attributes[7]),
+			'component_path' => WP_CONTENT_URL . "/{$plugin_dir}/",
+			'swf_name' => 'accordionMenu.swf',
+		);
+		$swf_embed['swf_path'] = $swf_embed['component_path'].$swf_embed['swf_name'];
+
+		if (!is_feed()) {
+			$embed_code = '<div id="accordionmenu-fx'.$accordionmenufx_params['count'].'">'.$swf_embed['text'].'</div>';
+			$embed_code .= '<script type="text/javascript">';
+			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'accordionmenu-fx{$accordionmenufx_params['count']}', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['component_path']}'".($settings_file_name != 'settings.xml' ? ", settingsXML: '{$settings_file_name}'" : '')." }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
+			$embed_code.= '</script>';
+		} else {
+			$embed_code = '<object width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'">';
+			$embed_code .= '<param name="movie" value="'.$swf_embed['swf_path'].'"></param>';
+			$embed_code .= '<param name="scale" value="noscale"></param>';
+			$embed_code .= '<param name="salign" value="tl"></param>';
+			$embed_code .= '<param name="wmode" value="transparent"></param>';
+			$embed_code .= '<param name="allowScriptAccess" value="sameDomain"></param>';
+			$embed_code .= '<param name="allowFullScreen" value="true"></param>';
+			$embed_code .= '<param name="sameDomain" value="true"></param>';
+			$embed_code .= '<param name="flashvars" value="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.$settings_file_name : '').'"></param>';
+			$embed_code .= '<embed type="application/x-shockwave-flash" width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'" src="'.$swf_embed['swf_path'].'" scale="noscale" salign="tl" wmode="transparent" allowScriptAccess="sameDomain" allowFullScreen="true" flashvars="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.$settings_file_name : '').'"';
+			$embed_code .= '></embed>';
+			$embed_code .= '</object>';
+		}
+
+		return $embed_code;
+	}
+
+	function accordionmenufx_filter_content($content) {
+		return preg_replace_callback('|\[accordion-menu-fx\s*(settings="([^"]+)")?\s*(width="([0-9]+)")?\s*(height="([0-9]+)")?\s*\](.*)\[/accordion-menu-fx\]|i', 'accordionmenufx_get_embed_code', $content);
+	}
+
+	function accordionmenufx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
+		echo accordionmenufx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
+	}
+
+	function accordionmenufx_load_swfobject_lib() {
+		wp_enqueue_script('swfobject');
+	}
+/* end client side functions */
+
+/* start admin section functions */
+	function accordionmenufx_admin_menu() {
+		add_options_page('Accordion Menu FX Options', 'Accordion Menu FX', 'manage_options', 'accordionmenufx', 'accordionmenufx_admin_options');
+	}
+
+	function accordionmenufx_admin_options() {
+		  if (!current_user_can('manage_options'))  {
+	    wp_die(__('You do not have sufficient permissions to access this page.'));
+	  }
+
+	  $accordionmenufx_default_path = get_option('accordionmenufx_path');
+	  if ($accordionmenufx_default_path === false) {
+	  	$accordionmenufx_default_path = 'flashxml/accordion-menu-fx';
+	  }
+?>
+<div class="wrap">
+	<h2>Accordion Menu FX</h2>
+	<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options'); ?>
+
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row" style="width: 40em;">SWF and assets path is <?php echo WP_CONTENT_DIR; ?>/</th>
+				<td><input type="text" style="width: 25em;" name="accordionmenufx_path" value="<?php echo $accordionmenufx_default_path; ?>" /></td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="accordionmenufx_path" />
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
+<?php
+	}
+/* end admin section functions */
+
+/* start hooks */
+	add_filter('the_content', 'accordionmenufx_filter_content');
+	add_action('init', 'accordionmenufx_load_swfobject_lib');
+	add_action('admin_menu', 'accordionmenufx_admin_menu');
+/* end hooks */
+
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Accordion Menu FX ===
+Contributors: flashxml
+Tags: images, photos, widget, post, plugin, posts, sidebar, free, flash, as3, accordion, menu, xml, text, effects, animation, vertical, horizontal, roll, over, out, button, transition, multiple, level
+Requires at least: 2.8.0
+Tested up to: 3.0.1
+Stable tag: trunk
+
+== Description ==
+
+Probably the most advanced Accordion Menu on the web. Fully XML customizable, without using Flash. And it's free!
+
+= Main features =
+
+You can place this Accordion Menu on any website for free without even using Flash. There is the possibility to have multiple levels menu with specified text orientation and alignment. There are roll over/out animation effects. The text is HTML/CSS formatted and you can embed any font. You can specify an action for each item. Many other customizable properties can be found using the Live Demo.
+
+== Installation ==
+
+Make sure your Wordpress version is greater than 2.8 and your hosting provider is using PHP5.
+
+1. There are two files to download: [WordPress Plugin](http://downloads.wordpress.org/plugin/accordion-menu-fx.zip "Accordion Menu FX Plugin") (that you have to install and activate) & [Free archive](http://www.flashxml.net/free/download/accordion-menu.zip "Accordion Menu FX")
+2. Create a new folder inside your **wp-content** folder called **flashxml**, inside this folder create a new one called **accordion-menu-fx** and copy the content of the **free archive** there
+3. If you copied the **free archive** to a location different than the one above, go to **Accordion Menu FX** from the **Settings** tab in your **WordPress Dashboard** and update the path accordingly
+4. Add `[accordion-menu-fx][/accordion-menu-fx]` where you want the Flash to show up in your post/page
+5. If you want to make the Accordion Menu FX part of your theme, edit the template files and add `<?php accordionmenufx_echo_embed_code(); ?>` where you want it to show up
+6. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Accordion Menu FX](http://www.flashxml.net/accordion-menu.html "Accordion Menu FX") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `wp-content/flashxml/accordion-menu-fx/settings.xml`
+
+= Additional settings file =
+
+To embed the Accordion Menu FX more than once, you will need another settings file. Let's assume your new file is called `settings2.xml`. Add `[accordion-menu-fx settings="settings2.xml"][/accordion-menu-fx]` where you want the Flash to show up in your post/page. If you made the Flash part of your theme, add the file name as **the first argument** of the `accordionmenufx_echo_embed_code()` function call (for example `<?php accordionmenufx_echo_embed_code("settings2.xml"); ?>`).
+
+= No Flash support text =
+
+To support visitors without Adobe Flash Player, you can provide alternative content by adding the text between `[accordion-menu-fx]` and `[/accordion-menu-fx]`. If you made the Flash part of your theme, add the text as **the second argument** of the `accordionmenufx_echo_embed_code()` function call (for example `<?php accordionmenufx_echo_embed_code("","Alternative content"); ?>`).
+
+= If you have PHP4 =
+
+To make it work with PHP4, add `[accordion-menu-fx width="600" height="300"][/accordion-menu-fx]` where you want the Flash to show up in your post/page. If you made the Flash part of your theme, add the width and height as **the third and fourth argument** of the `accordionmenufx_echo_embed_code()` function call. Don't forget to provide your own width and height values, since 600 and 300 are just examples.
+
+= Getting rid of the FlashXML.net label =
+
+To remove the FlashXML.net label from the top-left corner you'll need to buy the [commercial archive](http://www.flashxml.net/accordion-menu.html#swmi-license "Accordion Menu FX"). Once you'll do that, simply use the SWF file from the commercial archive to overwrite the SWF file from the `wp-content/flashxml/accordion-menu-fx/` folder.
+
+== Screenshots ==
+
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/accordion-menu.html "Accordion Menu FX") is the utility that helps easily customize your Accordion Menu FX to fit all your needs.
